2e4054b3c9f45f43b1418e1188fab1ce297f9458
yuanmingxiang <yuanmingxiang@querycap.com>
2022-08-24 17:01:57 +0800

    feat: chechbox done

---
+++ components/checkbox/Checkbox.tsx
@@ -0,0 +1,4 @@
+import Checkbox from 'antd/lib/checkbox';
+import './index.less';
+
+export default Checkbox;
--- components/index.tsx
+++ components/index.tsx
@@ -3,5 +3,6 @@ import './style/index.less';
 // export type { ButtonProps } from "./button";
 export { default as Breadcrumb } from './breadcrumb';
 export { default as Button } from './button';
+export { default as Checkbox } from './checkbox';
 export { default as Menu } from './menu';
 export { default as Radio } from './radio';
